e283782e3c995cbb32974aea7e89dcd7aa25956c
“akashad48” <akashad48@gmail.com>
2023-08-14 11:28:01 +0530

    pojos added

---
+++ backend/src/main/java/com/heavyrent/pojo/Document.java
@@ -0,0 +1,30 @@
+package com.heavyrent.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="document_tbl")
+public class Document {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	    private String adhar;
+	 
+	 
+	    private String pan;
+	    
+	 @OneToOne(mappedBy="document")
+	 private User user;
+
+	
+	 
+	 
+
+}
