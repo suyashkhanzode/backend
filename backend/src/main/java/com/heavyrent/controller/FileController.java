da6cce20704406918e1726ecdb9d797d4f4accd4
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2023-08-16 00:17:14 +0530

    Image uploading and downloading

--- backend/src/main/java/com/heavyrent/controller/FileController.java
+++ backend/src/main/java/com/heavyrent/controller/FileController.java
@@ -1,10 +1,15 @@
 package com.heavyrent.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,10 +22,10 @@ public class FileController {
 	@Autowired
 	private FileService service;
 	
-	@PostMapping(value = "/{eqpid}/image1",consumes = "multipart/form-data")
-	public String uploadImg1(@PathVariable long eqpid, @RequestBody MultipartFile img1)
+	@PostMapping(value = "/{eqpid}/image1",consumes = MediaType.ALL_VALUE)
+	public String uploadImg1(@PathVariable long eqpid, @RequestParam("img1") MultipartFile img1) throws  IOException
 	{
-		 String msg =  service.uploadImg(eqpid, img1);
+		 String msg =  service.uploadImg1(eqpid, img1);
 		 
 		 if(msg != null)
 		 {
@@ -29,6 +34,21 @@ public class FileController {
 		 else {
 			 return "File Not Uploaded";
 		 }
+	}
+	
+	@GetMapping(value = "/image1/{eqpid}",produces = MediaType.ALL_VALUE )
+	public byte[] doenloadImg1(@PathVariable long eqpid) throws IOException 
+	{
+		       byte[] img =   service.downloadImg1(eqpid);
+		       
+		       if(img != null)
+		       {
+		    	   return img;
+		       }
+		       else {
+		            return null;
+		       }
+		
 	}
 
 }
